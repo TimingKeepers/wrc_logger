c2c10344e2774a79ad3bddb5eb431284c4023c98
Felipe Torres <torresfelipex1@gmail.com>
2016-01-21 14:57:40 +0100

    added verbose output, fixed bug in sync checking

--- check_status.py
+++ check_status.py
@@ -28,7 +28,7 @@ import re
 import argparse as arg
 
 # Get servo state
-SS_REGEX   = 'ss:\'(\w+)\''
+SS_REGEX   = 'ss:\'(\w*)\''
 TEMP_REGEX = '\d{1,3}\.\d{4}'
 
 
@@ -71,36 +71,38 @@ class Log_Checker():
         for line in self.file:
             servostate = self.ss_regex.search(line)
             if servostate is not None:
-                ss = servostate.group(1)
-            if ss != state and servostate is not None:
-                print(line)
-                sync_lost += 1
+                if servostate.group(1) != state:
+                    sync_lost += 1
 
         return sync_lost
 
-    def check_temp(self, min, max):
+    def check_temp(self, min, max, verbose=False):
         '''
         Method to check the temperature of the board
 
         Args:
             min (int) : Minimal value allowed
             max (int) : Maximum value allowed
+            verbose (bool) : Adds to the returned value the mean temperature
 
         Returns:
             How many times temp is out of range
         '''
         temp_out = 0
         t = "0"
+        m_t = 0
+        i = 0
 
         for line in self.file:
             temp = self.temp_regex.search(line)
             if temp is not None:
                 t = temp.group(0)
+                m_t += float(t)
+                i += 1
             if not (min < float(t) < max) and temp is not None:
-                print("Temp out of range : %s" % t)
                 temp_out += 1
 
-        return temp_out
+        return (temp_out,) if not verbose else (temp_out, m_t/i)
 
 
 def main():
@@ -111,6 +113,8 @@ def main():
     action="store_true")
     parser.add_argument('-t','--temp', help=("Check temp range [min,max]"), \
     nargs='?',type=str)
+    parser.add_argument('-v','--verbose', help=("Enable verbose mode"), \
+    action="store_true", default=False)
 
     args = parser.parse_args()
 
@@ -122,18 +126,23 @@ def main():
         if not sync_failures:
             print("The WR synchronization is OK")
         else:
-            print("WR sync have been lost (%d) times" % sync_failures)
+            print("WR sync is lost")
     if args.temp is not None:
         temp = [float(i) for i in args.temp.split(",")]
         if len(temp) >= 2:
-            temp_failures = lc.check_temp(temp[0], temp[1])
-            if not temp_failures:
+            temp_failures = lc.check_temp(temp[0], temp[1], args.verbose)
+            if not temp_failures[0] and not args.verbose:
                 print("The temperature was in range")
+            elif not temp_failures[0] and args.verbose:
+                print("The temperature was in range : %f" % (temp_failures[1]))
             else:
-                print("Temperature was out of range %d times" % temp_failures)
+                print("Temperature was out of range:")
+                if args.verbose:
+                    print("Mean temperature : %.2f ºC (range=[%.2f,%.2f])" % \
+                    (temp_failures[1],temp[0],temp[1]))
         else: print("Error: Temp checking needs 2 parameters: min, max")
 
-    return (sync_failures+temp_failures)
+    return (sync_failures+temp_failures[0])
 
 if __name__ == '__main__':
     main()
